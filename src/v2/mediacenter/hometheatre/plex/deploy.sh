44db90d1666bad3a2074ad77456f908ad7edef7a
6ameDev <sumitdas.usit@gmail.com>
2022-10-29 20:08:21 +0530

    Adds auto updating to plex and portainer whenever they are deployed

--- src/v2/mediacenter/hometheatre/plex/deploy.sh
+++ src/v2/mediacenter/hometheatre/plex/deploy.sh
@@ -12,8 +12,14 @@ sudo chown -R $(id -u):mediacenter ${APP_CONFIG_DIR}/plex
 # Export current user's id to UID environment variable
 echo "UID=$(id -u)" >> .env
 
-# Deploy docker container
-# docker compose up -d
+# Pull latest docker images for all the required services.
+docker compose pull
+
+# Run Docker Compose to get all the required services up and running.
+docker compose --compatibility up -d
+
+# Clean up stale docker images
+docker image prune -f
 
 # Deploy docker stack hometheatre based on plex
 # docker stack deploy -c docker-compose-stack.yml hometheatre
