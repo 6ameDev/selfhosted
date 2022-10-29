16e979f69c039b921aa8c6df28559b1dfd551f5c
6ameDev <sumitdas.usit@gmail.com>
2022-10-29 21:14:42 +0530

    Migrates jellyfin to v2

--- src/v2/mediacenter/hometheatre/jellyfin/deploy.sh
+++ src/v2/mediacenter/hometheatre/jellyfin/deploy.sh
@@ -12,5 +12,11 @@ sudo chown -R $(id -u):mediacenter ${APP_CONFIG_DIR}/jellyfin
 # Export current user's id to UID environment variable
 echo "UID=$(id -u)" >> .env
 
-# Deploy docker stack hometheatre based on plex
-docker stack deploy -c docker-compose-stack.yml hometheatre
+# Pull latest docker images for all the required services.
+docker compose pull
+
+# Run Docker Compose to get all the required services up and running.
+docker compose --compatibility up -d
+
+# Clean up stale docker images
+docker image prune -f
