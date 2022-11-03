ba73d4c4cd9248f2a18d827bdc49c221ca8a1f07
6ameDev <sumitdas.usit@gmail.com>
2022-11-04 03:01:11 +0530

    Replaces postgres with MariaDb for nextcloud

--- src/nextcloud/deploy.sh
+++ src/nextcloud/deploy.sh
@@ -4,11 +4,13 @@
 source ./../.env
 
 # Make directories
-sudo mkdir -pv ${APP_CONFIG_DIR}/{nextcloud,postgres}
+sudo mkdir -pv ${APP_CONFIG_DIR}/nextcloud/{main,apps,config,theme}
+sudo mkdir -pv ${APP_CONFIG_DIR}/mariadb
 
 # Set permissions
-sudo chown -R $(id -u):$(id -u) ${APP_CONFIG_DIR}/nextcloud
-sudo chown -R $(id -u):$(id -u) ${APP_CONFIG_DIR}/postgres
+sudo chown -R www-data:$(id -u) ${APP_CONFIG_DIR}/nextcloud
+sudo chown -R www-data:$(id -u) ${DATA_DIR}
+sudo chown -R $(id -u):$(id -u) ${APP_CONFIG_DIR}/mariadb
 
 # Pull latest docker images for all the required services.
 docker compose pull
