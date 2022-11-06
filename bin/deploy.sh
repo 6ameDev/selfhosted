e9012d4f21c822bca60d6fadf3f9898d962ca471
6ameDev <sumitdas.usit@gmail.com>
2022-11-06 19:19:45 +0530

    Adds portainer to the ci pipeline

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,23 +2,22 @@
 
 # Load base environment variables
 source ./.env
-source ./../.env
 
 # Pull latest docker images for all the required services.
 docker compose pull
 
 # Deploy individual stacks
 if [ "$MEDIACENTER" != 'None' ]; then
-    ./deploy_mediacenter.sh
+    ./bin/deploy_mediacenter.sh
 fi
 if [ "$ENABLE_NEXTCLOUD" = true ]; then
-    ./deploy_nextcloud.sh
+    ./bin/deploy_nextcloud.sh
 fi
 if [ "$ENABLE_CHANGEDETECTION" = true ]; then
-    ./deploy_changed.sh
+    ./bin/deploy_changed.sh
 fi
 if [ "$ENABLE_PORTAINER" = true ]; then
-    ./deploy_portainer.sh
+    ./bin/deploy_portainer.sh
 fi
 
 # Clean up stale docker images
--- bin/deploy_portainer.sh
+++ bin/deploy_portainer.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Do something here!"
+echo "App Config Dir: ${APP_CONFIG_DIR}"
+
+# Make users and groups
+sudo useradd portainer -u 13200
+
+# Make directories
+sudo mkdir -pv ${APP_CONFIG_DIR}/portainer
+
+# Set permissions
+sudo chown -R portainer:portainer ${APP_CONFIG_DIR}/portainer
+
+# Run Docker Compose to get all the required services up and running.
+docker compose --compatibility up -d
