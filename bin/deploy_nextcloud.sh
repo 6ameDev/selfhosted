407b6cb3d1edac950514a8056c2a73afab4329a2
6ameDev <sumitdas.usit@gmail.com>
2022-11-06 20:36:27 +0530

    Enables nextcloud in the ci script

--- bin/deploy_nextcloud.sh
+++ bin/deploy_nextcloud.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Do something here!"
+## Make directories
+sudo mkdir -pv ${APP_CONFIG_DIR}/nextcloud/{main,apps,config,theme}
+sudo mkdir -pv ${APP_CONFIG_DIR}/postgres
+
+## Set permissions
+# www-data user must own the nextcloud directories
+sudo chown -R www-data:$(id -u) ${APP_CONFIG_DIR}/nextcloud
+sudo chown -R www-data:$(id -u) ${DATA_DIR}
+
+# 999 is the postgres user
+sudo chown -R 999:$(id -u) ${APP_CONFIG_DIR}/postgres
+
+## Pull latest docker images for all the required services.
+docker compose pull
+
+## Run Docker Compose to get all the required services up and running.
+docker compose --compatibility up -d
