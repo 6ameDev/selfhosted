6bc8ef378d580998b80a0889b0b2a332e8c6d884
6ameDev <sumitdas.usit@gmail.com>
2022-11-06 17:44:45 +0530

    Adds sample env file and deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "Do something here!"
+# Load base environment variables
+source ./../.env
+
+# Pull latest docker images for all the required services.
+docker compose pull
+
+# Deploy individual stacks
+if [ "$MEDIACENTER" != 'None' ]; then
+    ./deploy_mediacenter.sh
+fi
+if [ "$ENABLE_NEXTCLOUD" = true ]; then
+    ./deploy_nextcloud.sh
+fi
+if [ "$ENABLE_CHANGEDETECTION" = true ]; then
+    ./deploy_changed.sh
+fi
+if [ "$ENABLE_PORTAINER" = true ]; then
+    ./deploy_portainer.sh
+fi
+
+# Clean up stale docker images
+docker image prune -f
