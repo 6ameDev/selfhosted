960a1548d10f9c54f88688e3b5f722ee5af41185
6ameDev <sumitdas.usit@gmail.com>
2022-11-10 23:47:18 +0530

    Updates nextcloud and mediacenter compose files

--- bin/deploy_mediacenter.sh
+++ bin/deploy_mediacenter.sh
@@ -29,3 +29,35 @@ sudo chmod -R 775 ${STORAGE_DIR}/torrents/
 sudo chmod -R 775 ${STORAGE_DIR}/media/
 sudo chown -R $(id -u):mediacenter ${STORAGE_DIR}/torrents
 sudo chown -R $(id -u):mediacenter ${STORAGE_DIR}/media
+
+## JELLYFIN
+# Make directories
+sudo mkdir -pv ${APP_CONFIG_DIR}/jellyfin
+
+# Set permissions
+sudo chown -R $(id -u):mediacenter ${APP_CONFIG_DIR}/jellyfin
+
+# Export current user's id to UID environment variable
+echo "UID=$(id -u)" >> .env
+
+# Pull latest docker images for all the required services.
+docker compose pull
+
+# Run Docker Compose to get all the required services up and running.
+docker compose --compatibility up -d
+
+## PLEX
+# Make directories
+sudo mkdir -pv ${APP_CONFIG_DIR}/plex
+
+# Set permissions
+sudo chown -R $(id -u):mediacenter ${APP_CONFIG_DIR}/plex
+
+# Export current user's id to UID environment variable
+echo "UID=$(id -u)" >> .env
+
+# Pull latest docker images for all the required services.
+docker compose pull
+
+# Run Docker Compose to get all the required services up and running.
+docker compose --compatibility up -d
