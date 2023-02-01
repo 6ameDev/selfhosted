aaa05d9eb0ad07bbb7f425de5e1e97218d37723b
Sumit Das <6ameDev@users.noreply.github.com>
2023-02-02 01:40:44 +0530

    Adds a temp docker.env file for jellyfin and plex (#16)

--- bin/scripts/deploy_plex.sh
+++ bin/scripts/deploy_plex.sh
@@ -3,9 +3,9 @@
 # Load base environment variables
 source ./.env
 
-#######
-## PLEX
-#######
+# UID will be appended to the temporary docker.env file
+# Adding to .env and sourcing it will give warning
+cp .env docker.env
 
 # Make directories
 sudo mkdir -pv ${APP_CONFIG_DIR}/plex
@@ -14,12 +14,14 @@ sudo mkdir -pv ${APP_CONFIG_DIR}/plex
 sudo chown -R $(id -u):mediacenter ${APP_CONFIG_DIR}/plex
 
 # Export current user's id to UID environment variable
-echo "UID=$(id -u)" >> .env
+echo "UID=$(id -u)" >> docker.env
 
 compose_file_path=src/mediacenter/hometheatre/plex/docker-compose.yml
 
 # Pull latest docker images for all the required services.
-docker compose -f $compose_file_path --env-file .env pull
+docker compose -f $compose_file_path --env-file docker.env pull
 
 # Run Docker Compose to get all the required services up and running.
-docker compose --compatibility -f $compose_file_path --env-file .env up -d
+docker compose --compatibility -f $compose_file_path --env-file docker.env up -d
+
+rm docker.env
