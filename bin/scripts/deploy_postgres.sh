8ae7a8cd6a21141427008453c3833dcf9ba5dd46
6ameDev <sumitdas.usit@gmail.com>
2023-02-06 23:44:11 +0530

    Adds missing env file config in the docker compose command

--- bin/scripts/deploy_postgres.sh
+++ bin/scripts/deploy_postgres.sh
@@ -9,7 +9,7 @@ cp src/datastores/postgres/db_init.sh ${APP_CONFIG_DIR}/postgres/init/db_init.sh
 compose_file_path=src/datastores/postgres/docker-compose.yml
 
 # Pull latest docker images for all the required services.
-docker compose -f $compose_file_path pull
+docker compose -f $compose_file_path --env-file .env pull
 
 # Run Docker Compose to get all the required services up and running.
-docker compose --compatibility -f $compose_file_path up -d
+docker compose --compatibility -f $compose_file_path --env-file .env up -d
