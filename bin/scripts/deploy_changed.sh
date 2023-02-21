8c207b0501f3089d36c4a34096ece06b6e645d6b
6ameDev <sumitdas.usit@gmail.com>
2023-02-22 01:20:07 +0530

    Cleans up container names for tunnels and ChangeD apps

--- bin/scripts/deploy_changed.sh
+++ bin/scripts/deploy_changed.sh
@@ -11,12 +11,3 @@ docker compose -f $compose_file_path pull
 
 # Run Docker Compose to get all the required services up and running.
 docker compose --compatibility -f $compose_file_path up -d
-
-## Deploying Apprise
-compose_file_path=src/tools/notification/docker-compose.yml
-
-# Pull latest docker images for all the required services.
-docker compose -f $compose_file_path --env-file .env pull
-
-# Run Docker Compose to get all the required services up and running.
-docker compose --compatibility -f $compose_file_path --env-file .env up -d
