cc8741a2807fb48fc0e7d461134669dec829d86f
6ameDev <sumitdas.usit@gmail.com>
2025-01-12 21:08:33 +0530

    Removed mediacenter docker files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,24 +4,24 @@
 source ./.env
 
 # Deploy individual stacks
-if [ "$MEDIACENTER" != 'None' ]; then
-    ./bin/scripts/deploy_mediacenter.sh
-fi
+# if [ "$MEDIACENTER" != 'None' ]; then
+#     ./bin/scripts/deploy_mediacenter.sh
+# fi
 if [ "$ENABLE_GHOSTFOLIO" = true ]; then
     ./bin/scripts/deploy_ghostfolio.sh
 fi
-if [ "$ENABLE_NEXTCLOUD" = true ]; then
-    ./bin/scripts/deploy_nextcloud.sh
-fi
+# if [ "$ENABLE_NEXTCLOUD" = true ]; then
+#     ./bin/scripts/deploy_nextcloud.sh
+# fi
 if [ "$ENABLE_CHANGEDETECTION" = true ]; then
     ./bin/scripts/deploy_changed.sh
 fi
 if [ "$ENABLE_NGINX_PROXY_MANAGER" = true ]; then
     ./bin/scripts/deploy_nginx_proxy_manager.sh
 fi
-if [ "$ENABLE_TUNNEL_PRIME" = true ]; then
-    ./bin/scripts/deploy_tunnel.sh
-fi
+# if [ "$ENABLE_TUNNEL_PRIME" = true ]; then
+#     ./bin/scripts/deploy_tunnel.sh
+# fi
 if [ "$ENABLE_PORTAINER" = true ]; then
     ./bin/scripts/deploy_portainer.sh
 fi
